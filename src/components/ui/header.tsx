274f833be8a186753942928dedd56f1176d00665
amalhanaja <amalhanaja@gmail.com>
2025-01-13 06:10:43 +0700

    feat: clerk integration

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -2,6 +2,9 @@
 
 import Link from "next/link";
 import { ThemeToggle } from "../theme-toggle";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Button } from "./button";
+import { Loader2 } from "lucide-react";
 
 export const Header = () => {
   return (
@@ -12,8 +15,16 @@ export const Header = () => {
       >
         CEPAK
       </Link>
-      <div>
+      <div className="flex gap-2 items-center">
         <ThemeToggle />
+        <SignedOut>
+          <Button asChild>
+            <Link href="/auth/sign-in">Sign In</Link>
+          </Button>
+        </SignedOut>
+        <SignedIn>
+          <UserButton fallback={<Loader2 className="animate-spin" />} />
+        </SignedIn>
       </div>
     </header>
   );
